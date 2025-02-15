f550e1cf98d8113dec5fa4f5f7ae7ea71d4a4ca5
Elilaw179 <sundayelisha179@gmail.com>
2025-02-15 15:46:51 +0100

    ExerciseTracking assignment done

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        // Create activities
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("03 Nov 2022", 30, 4.8),
+            new Cycling("03 Nov 2022", 45, 25.0),
+            new Swimming("03 Nov 2022", 20, 30)
+        };
+
+        // Display summaries
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
